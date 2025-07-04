772e03ca5aa751961c64b4f9b614e126d180e714
Takkyoui <takelucky5659@gmail.com>
2025-07-04 14:08:13 +0900

    API URL을 로컬호스트에서 https://api.improfessor.co.kr로 변경

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://api.improfessor.co.kr/',
   headers: {
     'Content-Type': 'application/json',
   },
