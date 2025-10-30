728dc6b5e3b1b2f6e4b93e730342320f28c74946
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-10-30 14:22:40 +0900

    [deploy] prod server url 변경

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://api.improfessor.kro.kr',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://prod.improfessor.kro.kr',
   headers: {
     'Content-Type': 'application/json',
   },
